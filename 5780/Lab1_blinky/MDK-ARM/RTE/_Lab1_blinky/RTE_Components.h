e4a3d719b5386440695b6b182f229678ca63b7ca
shem-snow <78665574+shem-snow@users.noreply.github.com>
2024-02-02 07:45:46 -0700

    started lab 1

---
+++ 5780/Lab1_blinky/MDK-ARM/RTE/_Lab1_blinky/RTE_Components.h
@@ -0,0 +1,15 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'Lab1_blinky' 
+ * Target:  'Lab1_blinky' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+
+#endif /* RTE_COMPONENTS_H */
